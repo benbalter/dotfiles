5ce2738104090e652f913663f4f65f82894dd3fc
Ben Balter <ben.balter@github.com>
2015-10-05 13:25:26 -0400

    allow alliases to overwrite inits

--- lib/init.sh
+++ lib/init.sh
@@ -2,14 +2,14 @@
 # Init ZSH and the shell environment
 # Usage source script/init
 
-# Load libs
-source ~/.files/lib/globals.sh
-source ~/.files/lib/zsh.sh
-source ~/.files/lib/aliases.sh
-source ~/.iterm2_shell_integration.zsh
-
 # Init some stuff
 if brew command command-not-found-init > /dev/null; then eval "$(brew command-not-found-init)"; fi
 if which nodenv > /dev/null; then eval "$(nodenv init -)"; fi
 if which rbenv > /dev/null;  then eval "$(rbenv init -)"; fi
 if which fasd > /dev/null; then eval "$(fasd --init auto)"; fi
+
+# Load libs
+source ~/.files/lib/globals.sh
+source ~/.files/lib/zsh.sh
+source ~/.files/lib/aliases.sh
+source ~/.iterm2_shell_integration.zsh
