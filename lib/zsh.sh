136e984f91653220000f760fcc54451da5a9565a
Ben Balter <ben.balter@github.com>
2015-10-05 12:13:03 -0400

    break plugins into their own line

--- lib/zsh.sh
+++ lib/zsh.sh
@@ -8,6 +8,38 @@ ZSH_THEME="robbyrussell"
 DISABLE_UPDATE_PROMPT=true
 COMPLETION_WAITING_DOTS="true"
 
-plugins=(atom battery bower brew bundler cloudapp coffee colored-man colorize command-not-found common-aliases cp extract fasd gem history-substring-search git git-extras heroku iwhois node npm osx rake rake-fast rbenv ruby safe-paste ssh-agent sudo zsh_reload)
+plugins=(
+  atom
+  battery
+  bower
+  brew
+  bundler
+  cloudapp
+  coffee
+  colored-man
+  colorize
+  command-not-found
+  common-aliases
+  cp
+  extract
+  fasd
+  gem
+  history-substring-search
+  git
+  git-extras
+  heroku
+  iwhois
+  node
+  npm
+  osx
+  rake
+  rake-fast
+  rbenv
+  ruby
+  safe-paste
+  ssh-agent
+  sudo
+  zsh_reload
+)
 
 source $ZSH/oh-my-zsh.sh
