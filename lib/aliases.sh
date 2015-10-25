7d3d4bbe99c95bcdba94176da23c91ef6381a9b2
Ben Balter <ben.balter@github.com>
2015-10-25 18:00:21 -0400

    add gccd

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -16,6 +16,7 @@ alias gb="git browse"
 alias reset="git reset HEAD --hard; git clean -f -d"
 alias clean="git branch --merged | grep -v '\*' | xargs -n 1 git branch -d"
 alias ggb="git commit -m ':gem: bump'"
+function gccd(){ git clone $1; cd $(basename $1); }
 
 # fasd
 alias j='fasd_cd -d'
