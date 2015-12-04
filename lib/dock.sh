e9e58edf7820d6a4ed62ccaabf36df70e69a07aa
Ben Balter <ben.balter@github.com>
2015-12-03 21:21:05 -0500

    Update dock.sh

--- lib/dock.sh
+++ lib/dock.sh
@@ -3,6 +3,9 @@ dockutil --list | awk -F"\t" '{print $1}' | while read line; do
   dockutil --remove "$line"
 done
 
+# Give dock time to restart
+sleep 1
+
 # Apps wanted in the dock, in order
 apps=(Adium Atom "Google Chrome" Slack Spotify)
 
