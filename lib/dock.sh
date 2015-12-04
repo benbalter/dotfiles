96428d0db65f617011044688d539a0d63bc22974
Ben Balter <ben.balter@github.com>
2015-12-03 21:12:39 -0500

    Create dock.sh

---
+++ lib/dock.sh
@@ -0,0 +1,12 @@
+# Remove all apps from the dock
+dockutil --list | awk -F"\t" '{print $1}' | while read line; do 
+  dockutil --remove "$line"
+done
+
+# Apps wanted in the dock, in order
+apps=(Adium Atom "Google Chrome" Slack Spotify)
+
+# Re-add the apps we want
+for app in $apps; do 
+  dockutil --add "/Applications/$app.app"
+done
