c6ee726bdf21c311614b0dc359c0e34fca684ed9
Ben Balter <ben.balter@github.com>
2015-12-29 16:27:53 -0500

    disable flash in chrome, fixes #16

---
+++ lib/chrome.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# Sets Chrome preferences
+
+require 'json'
+require 'pp'
+
+file = File.expand_path "~/Library/Application Support/Google/Chrome/Default/Preferences"
+json = File.open(file).read
+prefs = JSON.parse(json)
+
+prefs["autofill"]["enabled"] = false
+prefs["enable_do_not_track"] = true
+prefs["profile"]["password_manager_enabled"] = false
+prefs["plugins"]["plugins_list"].select { |p| p["name"] =~ /\bFlash\b/}.each do |p|
+  p["enabled"] = false
+end
+
+File.write(file, prefs.to_json)
