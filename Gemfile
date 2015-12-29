5cf8752374f5c90bad97ee52f3d89462d4436b87
Ben Balter <ben.balter@github.com>
2015-12-29 18:16:26 -0500

    set up iterm, fixes #14, fixes #13, fixes #12

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'gem-release'
 gem 'pry'
+gem 'CFPropertyList'
---
+++ lib/iterm.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+# Sets iTerm2 preferences
+
+require 'CFPropertyList'
+require 'pp'
+
+# Top level preferences
+preferences = {
+  "Hotkey" => 1,
+  "HotkeyChar" => 96,
+  "HotkeyCode" => 50,
+  "HotkeyModifiers" => 262401,
+  "HotKeyTogglesWindow" => 1
+}
+
+# Per profile preferences
+profile_preferences = {
+  "Prompt Before Closing 2" => 0,
+  "Blend"                   => 0.5,
+  "Scrollback Lines"        => 0,
+  "Unlimited Scrollback"    => true,
+  "Normal Font"             => "Inconsolata 16",
+  "Non Ascii Font"          => "Inconsolata 16",
+  "Transparency"            => 0.30,
+  "Custom Directory"        => "Recycle"
+}
+
+file = File.expand_path "~/Library/Preferences/com.googlecode.iterm2.plist"
+plist = CFPropertyList::List.new file: file
+data = CFPropertyList.native_types(plist.value)
+
+hotkey_profile = data["New Bookmarks"].find { |p| p["Name"] == "Hotkey Window"}
+pp hotkey_profile
+preferences["HotKeyBookmark"] = hotkey_profile["Guid"]
+preferences.each do |key, value|
+  data[key] = value
+end
+
+data["New Bookmarks"].each do |profile|
+  profile_preferences.each do |key, value|
+    profile[key] = value
+  end
+end
+
+plist.value = CFPropertyList.guess(data, :convert_unknown_to_string => true)
+plist.save file, CFPropertyList::List::FORMAT_BINARY
