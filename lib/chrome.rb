aff11891df5c35612e17178cdfaaa43f2817b87a
Ben Balter <ben.balter@github.com>
2015-12-30 15:12:20 -0500

    enable warn before quiting mode for chrome, fixes #4

--- lib/chrome.rb
+++ lib/chrome.rb
@@ -3,15 +3,25 @@
 
 require 'json'
 
-file = File.expand_path "~/Library/Application Support/Google/Chrome/Default/Preferences"
-json = File.open(file).read
-prefs = JSON.parse(json)
-
-prefs["autofill"]["enabled"] = false
-prefs["enable_do_not_track"] = true
-prefs["profile"]["password_manager_enabled"] = false
-prefs["plugins"]["plugins_list"].select { |p| p["name"] =~ /\bFlash\b/}.each do |p|
-  p["enabled"] = false
+def set_prefs(file, &block)
+  file = File.expand_path(file)
+  json = File.open(file).read
+  prefs = JSON.parse(json)
+  yield(prefs)
+  File.write(file, prefs.to_json)
 end
 
-File.write(file, prefs.to_json)
+# Profile preferences
+set_prefs("~/Library/Application Support/Google/Chrome/Default/Preferences") do |prefs|
+  prefs["autofill"]["enabled"] = false
+  prefs["enable_do_not_track"] = true
+  prefs["profile"]["password_manager_enabled"] = false
+  prefs["plugins"]["plugins_list"].select { |p| p["name"] =~ /\bFlash\b/}.each do |p|
+    p["enabled"] = false
+  end
+end
+
+# App state preferences
+set_prefs("~/Library/Application Support/Google/Chrome/Local State") do |prefs|
+  prefs["browser"]["confirm_to_quit"] = true
+end
