b68d88b4a959785ae248f34ea368c7e79c349748
Ben Balter <ben.balter@github.com>
2016-01-16 17:07:19 -0500

    more os x settings

--- lib/osx.sh
+++ lib/osx.sh
@@ -15,6 +15,8 @@ defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled -bool false
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabledForLocalFiles -bool false
 defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
 # Software update
 sudo defaults write /Library/Preferences/com.apple.commerce AutoUpdate -bool TRUE
@@ -31,5 +33,16 @@ defaults -currentHost write com.apple.ScreenSaverPhotoChooser SelectedFolderPath
 defaults -currentHost write com.apple.ScreenSaverPhotoChooser CustomFolderDict -dict identifier "/Users/benbalter/github/octodex.github.com/images" name "octodex"
 defaults -currentHost write com.apple.ScreenSaverPhotoChooser SelectedFolderPath "/Users/benbalter/github/octodex.github.com/images"
 defaults -currentHost write com.apple.ScreenSaverPhotoChooser SelectedSource -int 4
+
+# SNap to grid
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+# Enable safari webinspector
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 touch "$HOME_DIR/.hushlogin"
