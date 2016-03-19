65578b4df9c3b5bc349a85e09a7b197c0eaa4b9c
Ben Balter <ben.balter@github.com>
2016-03-19 15:13:06 -0400

    run rubocop

--- lib/chrome.rb
+++ lib/chrome.rb
@@ -3,7 +3,7 @@
 
 require 'json'
 
-def set_prefs(file, &block)
+def set_prefs(file)
   file = File.expand_path(file)
   json = File.open(file).read
   prefs = JSON.parse(json)
@@ -12,16 +12,16 @@ def set_prefs(file, &block)
 end
 
 # Profile preferences
-set_prefs("~/Library/Application Support/Google/Chrome/Default/Preferences") do |prefs|
-  prefs["autofill"]["enabled"] = false
-  prefs["enable_do_not_track"] = true
-  prefs["profile"]["password_manager_enabled"] = false
-  prefs["plugins"]["plugins_list"].select { |p| p["name"] =~ /\bFlash\b/}.each do |p|
-    p["enabled"] = false
+set_prefs('~/Library/Application Support/Google/Chrome/Default/Preferences') do |prefs|
+  prefs['autofill']['enabled'] = false
+  prefs['enable_do_not_track'] = true
+  prefs['profile']['password_manager_enabled'] = false
+  prefs['plugins']['plugins_list'].select { |p| p['name'] =~ /\bFlash\b/ }.each do |p|
+    p['enabled'] = false
   end
 end
 
 # App state preferences
-set_prefs("~/Library/Application Support/Google/Chrome/Local State") do |prefs|
-  prefs["browser"]["confirm_to_quit"] = true
+set_prefs('~/Library/Application Support/Google/Chrome/Local State') do |prefs|
+  prefs['browser']['confirm_to_quit'] = true
 end
--- lib/iterm.rb
+++ lib/iterm.rb
@@ -5,28 +5,28 @@ require 'CFPropertyList'
 
 # Per profile preferences
 profile_preferences = {
-  "Prompt Before Closing 2" => 0,
-  "Blend"                   => 0.5,
-  "Scrollback Lines"        => 0,
-  "Unlimited Scrollback"    => true,
-  "Normal Font"             => "Inconsolata 16",
-  "Non Ascii Font"          => "Inconsolata 16",
-  "Transparency"            => 0.30,
-  "Custom Directory"        => "Recycle"
+  'Prompt Before Closing 2' => 0,
+  'Blend'                   => 0.5,
+  'Scrollback Lines'        => 0,
+  'Unlimited Scrollback'    => true,
+  'Normal Font'             => 'Inconsolata 16',
+  'Non Ascii Font'          => 'Inconsolata 16',
+  'Transparency'            => 0.30,
+  'Custom Directory'        => 'Recycle'
 }
 
-file = File.expand_path "~/Library/Preferences/com.googlecode.iterm2.plist"
+file = File.expand_path '~/Library/Preferences/com.googlecode.iterm2.plist'
 plist = CFPropertyList::List.new file: file
 data = CFPropertyList.native_types(plist.value)
 
-hotkey_profile = data["New Bookmarks"].find { |p| p["Name"] == "Hotkey Window"}
-preferences["HotKeyBookmark"] = hotkey_profile["Guid"]
+hotkey_profile = data['New Bookmarks'].find { |p| p['Name'] == 'Hotkey Window' }
+preferences['HotKeyBookmark'] = hotkey_profile['Guid']
 
-data["New Bookmarks"].each do |profile|
+data['New Bookmarks'].each do |profile|
   profile_preferences.each do |key, value|
     profile[key] = value
   end
 end
 
-plist.value = CFPropertyList.guess(data, :convert_unknown_to_string => true)
+plist.value = CFPropertyList.guess(data, convert_unknown_to_string: true)
 plist.save file, CFPropertyList::List::FORMAT_BINARY
