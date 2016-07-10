3207ce5b2ab8a230f1b7eeb63e569f19ba04315b
Ben Balter <ben.balter@github.com>
2016-07-10 18:35:16 -0400

    add rubocop

--- lib/app-store.rb
+++ lib/app-store.rb
@@ -2,8 +2,8 @@
 
 require 'yaml'
 
-installed = `mas list`.split("\n").map { |line| line.split(" ").first.to_i }
-apps = YAML.load_file File.expand_path("~/.mas.yml")
+installed = `mas list`.split("\n").map { |line| line.split(' ').first.to_i }
+apps = YAML.load_file File.expand_path('~/.mas.yml')
 apps.each do |id, name|
   if installed.include? id
     puts "#{name} is already installed"
